8a6ccb28cbbfd4e2a60f45322330e3692fb7c3d5
Jinho An <jinhoan0422@gmail.com>
2024-05-04 18:07:44 +0200

    Initial Commit

---
+++ RecipeBrowser/RecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserApp.swift
+//  RecipeBrowser
+//
+//  Created by Jinho An on 04.05.24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
