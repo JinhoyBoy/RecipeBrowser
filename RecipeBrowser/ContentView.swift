bf2e84cf3f59bf6e6d9238906bc1362221daa56a
Jinho An <jinhoan0422@gmail.com>
2024-05-04 18:44:51 +0200

    Configured Firebase / first test and fetch

--- RecipeBrowser/ContentView.swift
+++ RecipeBrowser/ContentView.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+import FirebaseDatabase
 
 struct ContentView: View {
+    @State private var ref: DatabaseReference!
+    @State private var recipeName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "carrot.fill")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.orange)
+            Text(recipeName)
+        }
+        .onAppear{
+            ref = Database.database().reference()
+            ref.child("7000").child("Recipe Name").getData { error, snapshot in
+                    if let error = error {
+                        print("Error fetching data: \(error)")
+                    } else if let value = snapshot?.value as? String {
+                        let Name = value
+                        print("Recipe Name: \(Name)")
+                        recipeName = Name
+                    } else {
+                        print("Data is not in the expected format.")
+                    }
+                }
         }
         .padding()
     }
--- RecipeBrowser/RecipeBrowserApp.swift
+++ RecipeBrowser/RecipeBrowserApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    return true
+  }
+}
 
 @main
 struct RecipeBrowserApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+      
+    init() {
+      FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
